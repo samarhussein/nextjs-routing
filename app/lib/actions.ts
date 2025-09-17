ae4e05423126090f90b69b80ea8ccf60e8c24aa5
Samar Hussein <Samar@172-4-161-31.lightspeed.tpkaks.sbcglobal.net>
2025-09-17 16:06:23 +0200

    Handle error and not found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -26,14 +26,17 @@ export async function createInvoice(formData: FormData) {
   // Test it out:
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
-  revalidatePath("/dashboard/invoices");
-  redirect("/dashboard/invoices");
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 // Use Zod to update the expected types
@@ -49,18 +52,26 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
+  } catch (err) {
+    console.log(err);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  throw new Error("Failed to Delete Invoice");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (err) {
+    console.log(err);
+  }
   revalidatePath("/dashboard/invoices");
 }
